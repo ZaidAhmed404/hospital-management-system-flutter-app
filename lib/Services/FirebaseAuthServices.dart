8a5263d3f42020a4087c0827bdaa671bae194ec7
ZaidAhmed404 <professionalzaidahmed@gmail.com>
2024-02-10 19:29:05 +0500

    google, facebook signin added

--- lib/Services/FirebaseAuthServices.dart
+++ lib/Services/FirebaseAuthServices.dart
@@ -64,6 +64,7 @@ class FirebaseAuthServices {
     // Create a credential from the access token
     final OAuthCredential facebookAuthCredential =
         FacebookAuthProvider.credential(loginResult.accessToken!.token);
+    ;
 
     // Once signed in, return the UserCredential
     return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
