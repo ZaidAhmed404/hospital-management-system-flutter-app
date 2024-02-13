88cc21c9db2014d0fcb4e31dad6c0b2d2aa5505f
ZaidAhmed404 <professionalzaidahmed@gmail.com>
2024-02-13 23:38:16 +0500

    Sign In working

--- lib/Screens/BoardingScreen/BoardingScreen.dart
+++ lib/Screens/BoardingScreen/BoardingScreen.dart
@@ -62,108 +62,135 @@ class _BoardingScreenState extends State<BoardingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white.withOpacity(0.98),
       body: LoadingOverlay(
-        isLoading: isLoading,
-        color: Colors.black,
-        opacity: 0.5,
-        progressIndicator: Container(
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(10)),
-            child: const CircularProgressIndicator()),
-        child: FirebaseAuth.instance.currentUser != null
-            ? PageView.builder(
-                controller: _pageController,
-                onPageChanged: (index) {},
-                itemCount: boarding.length + 1,
-                itemBuilder: (_, index) {
-                  return index == boarding.length
-                      ? const WelcomeScreen()
-                      : Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height,
-                          alignment: Alignment.bottomCenter,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage(boarding[index]['image']))),
-                          child: Container(
-                            width: MediaQuery.of(context).size.width,
-                            height: MediaQuery.of(context).size.height * 0.4,
-                            padding: const EdgeInsets.all(20),
-                            decoration: const BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(40),
-                                    topRight: Radius.circular(40))),
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    boarding[index]['heading'],
-                                    style: const TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w800,
-                                        color: Colors.blue),
-                                  ),
-                                  Text(
-                                    boarding[index]['description'],
-                                    textAlign: TextAlign.center,
-                                    style: const TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
+          isLoading: isLoading,
+          color: Colors.black,
+          opacity: 0.5,
+          progressIndicator: Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              child: const CircularProgressIndicator()),
+          child: StreamBuilder(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder: (ctx, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    padding: const EdgeInsets.all(20),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Center(
+                          child: CircularProgressIndicator(),
+                        )
+                      ],
+                    ),
+                  );
+                }
+
+                if (snapshot.hasData) {
+                  return const Center(child: Text("Signed in"));
+                }
+
+                return PageView.builder(
+                    controller: _pageController,
+                    onPageChanged: (index) {},
+                    itemCount: boarding.length + 1,
+                    itemBuilder: (_, index) {
+                      return index == boarding.length
+                          ? const WelcomeScreen()
+                          : Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: MediaQuery.of(context).size.height,
+                              alignment: Alignment.bottomCenter,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: AssetImage(
+                                          boarding[index]['image']))),
+                              child: Container(
+                                width: MediaQuery.of(context).size.width,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.4,
+                                padding: const EdgeInsets.all(20),
+                                decoration: const BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(40),
+                                        topRight: Radius.circular(40))),
+                                child: Center(
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
                                     children: [
-                                      for (int i = 0; i < boarding.length; i++)
-                                        Container(
-                                          width: 5,
-                                          height: 5,
-                                          margin: const EdgeInsets.all(5),
-                                          decoration: BoxDecoration(
-                                              color: (i == index)
-                                                  ? Colors.black
-                                                  : Colors.black12,
-                                              shape: BoxShape.circle),
-                                        )
+                                      Text(
+                                        boarding[index]['heading'],
+                                        style: const TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w800,
+                                            color: Colors.blue),
+                                      ),
+                                      Text(
+                                        boarding[index]['description'],
+                                        textAlign: TextAlign.center,
+                                        style: const TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          for (int i = 0;
+                                              i < boarding.length;
+                                              i++)
+                                            Container(
+                                              width: 5,
+                                              height: 5,
+                                              margin: const EdgeInsets.all(5),
+                                              decoration: BoxDecoration(
+                                                  color: (i == index)
+                                                      ? Colors.black
+                                                      : Colors.black12,
+                                                  shape: BoxShape.circle),
+                                            )
+                                        ],
+                                      ),
+                                      TextButton(
+                                          onPressed: () {
+                                            _pageController
+                                                .jumpToPage(boarding.length);
+                                          },
+                                          child: const Text(
+                                            "Skip",
+                                            style: TextStyle(
+                                                color: Colors.blue,
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.w500),
+                                          )),
+                                      ButtonWidget(
+                                          buttonText: "Next",
+                                          buttonColor: Colors.blueAccent,
+                                          borderColor: Colors.blueAccent,
+                                          textColor: Colors.white,
+                                          onPressedFunction: () {
+                                            _pageController.nextPage(
+                                              duration: const Duration(
+                                                  milliseconds: 500),
+                                              curve: Curves.ease,
+                                            );
+                                          }),
                                     ],
                                   ),
-                                  TextButton(
-                                      onPressed: () {
-                                        _pageController
-                                            .jumpToPage(boarding.length);
-                                      },
-                                      child: const Text(
-                                        "Skip",
-                                        style: TextStyle(
-                                            color: Colors.blue,
-                                            fontSize: 15,
-                                            fontWeight: FontWeight.w500),
-                                      )),
-                                  ButtonWidget(
-                                      buttonText: "Next",
-                                      buttonColor: Colors.blueAccent,
-                                      borderColor: Colors.blueAccent,
-                                      textColor: Colors.white,
-                                      onPressedFunction: () {
-                                        _pageController.nextPage(
-                                          duration:
-                                              const Duration(milliseconds: 500),
-                                          curve: Curves.ease,
-                                        );
-                                      }),
-                                ],
+                                ),
                               ),
-                            ),
-                          ),
-                        );
-                })
-            : const Center(child: Text("Signed in")),
-      ),
+                            );
+                    });
+              })),
     );
   }
 }
