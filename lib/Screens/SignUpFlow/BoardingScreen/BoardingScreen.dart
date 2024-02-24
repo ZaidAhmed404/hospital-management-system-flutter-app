3c0a7d327e792de4824310521fd2663db226bad0
ZaidAhmed404 <professionalzaidahmed@gmail.com>
2024-02-24 12:15:54 +0500

    Boarding Screen changed

--- lib/Screens/SignUpFlow/BoardingScreen/BoardingScreen.dart
+++ lib/Screens/SignUpFlow/BoardingScreen/BoardingScreen.dart
@@ -24,10 +24,6 @@ class BoardingScreen extends StatefulWidget {
 }
 
 class _BoardingScreenState extends State<BoardingScreen> {
-  Map<String, dynamic> data = {};
-  bool gotCollectionData = false;
-  String role = "";
-  bool isLoading = false;
   List<Map> boarding = [
     {
       "image": "assets/images/boarding 1.jpg",
@@ -52,207 +48,95 @@ class _BoardingScreenState extends State<BoardingScreen> {
   final PageController _pageController = PageController(initialPage: 0);
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    fetchData(context: context);
-  }
-
-  Future fetchData({required BuildContext context}) async {
-    setState(() {
-      isLoading = true;
-    });
-    await FirebaseFirestore.instance
-        .collection('doctors')
-        .doc(FirebaseAuth.instance.currentUser?.uid)
-        .get()
-        .then((DocumentSnapshot documentSnapshot) {
-      if (documentSnapshot.exists) {
-        log("${documentSnapshot.data()}", name: "doctor data");
-        if (documentSnapshot.exists) {
-          data = documentSnapshot.data() as Map<String, dynamic>;
-        }
-      }
-    });
-    if (data.isNotEmpty) {
-      if (context.mounted) {
-        setState(() {
-          gotCollectionData = true;
-          role = "doctor";
-          BlocProvider.of<DoctorCubit>(context)
-              .updateDoctorModel(singleDoctorModel: DoctorModel.fromMap(data));
-        });
-      }
-    }
-    if (gotCollectionData == false) {
-      await FirebaseFirestore.instance
-          .collection('patients')
-          .doc(FirebaseAuth.instance.currentUser?.uid)
-          .get()
-          .then((DocumentSnapshot documentSnapshot) {
-        if (documentSnapshot.exists) {
-          log("${documentSnapshot.data()}", name: "patient data");
-
-          data = documentSnapshot.data() as Map<String, dynamic>;
-        }
-      });
-      if (data.isNotEmpty) {
-        if (context.mounted) {
-          setState(() {
-            gotCollectionData = true;
-            role = "patient";
-          });
-          BlocProvider.of<PatientCubit>(context).updatePatientModel(
-              singlePatientModel: PatientModel.fromMap(data));
-        }
-      }
-    }
-    if (context.mounted) {
-      BlocProvider.of<UserCubit>(context).updateUserModel(
-        email: FirebaseAuth.instance.currentUser!.email.toString(),
-        uid: FirebaseAuth.instance.currentUser!.uid.toString(),
-        displayName: FirebaseAuth.instance.currentUser!.displayName.toString(),
-        photoUrl: FirebaseAuth.instance.currentUser!.photoURL.toString(),
-      );
-    }
-    setState(() {
-      isLoading = false;
-    });
-
-    if (gotCollectionData && role == "doctor" && isLoading == false) {
-      if (context.mounted) {
-        Navigator.pushAndRemoveUntil(
-          context,
-          MaterialPageRoute(builder: (context) => DoctorLandingScreen()),
-          (route) => false, // Close all existing routes
-        );
-      }
-    } else if (gotCollectionData && role == "patient" && isLoading == false) {
-      if (context.mounted) {
-        Navigator.pushAndRemoveUntil(
-          context,
-          MaterialPageRoute(builder: (context) => PatientLandingScreen()),
-          (route) => false, // Close all existing routes
-        );
-      }
-      return PatientLandingScreen();
-    } else if (role == "" && isLoading == false) {
-      if (context.mounted) {
-        Navigator.pushAndRemoveUntil(
-          context,
-          MaterialPageRoute(builder: (context) => RegisterUserRoleScreen()),
-          (route) => false, // Close all existing routes
-        );
-      }
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: isLoading
-            ? Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                padding: const EdgeInsets.all(20),
-                child: const Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  ],
-                ),
-              )
-            : PageView.builder(
-                controller: _pageController,
-                onPageChanged: (index) {},
-                itemCount: boarding.length + 1,
-                itemBuilder: (_, index) {
-                  return index == boarding.length
-                      ? const WelcomeScreen()
-                      : Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height,
-                          alignment: Alignment.bottomCenter,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage(boarding[index]['image']))),
-                          child: Container(
-                            width: MediaQuery.of(context).size.width,
-                            height: MediaQuery.of(context).size.height * 0.4,
-                            padding: const EdgeInsets.all(20),
-                            decoration: const BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(40),
-                                    topRight: Radius.circular(40))),
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+        body: PageView.builder(
+            controller: _pageController,
+            onPageChanged: (index) {},
+            itemCount: boarding.length + 1,
+            itemBuilder: (_, index) {
+              return index == boarding.length
+                  ? const WelcomeScreen()
+                  : Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height,
+                      alignment: Alignment.bottomCenter,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage(boarding[index]['image']))),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * 0.4,
+                        padding: const EdgeInsets.all(20),
+                        decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(40),
+                                topRight: Radius.circular(40))),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                boarding[index]['heading'],
+                                style: const TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w800,
+                                    color: Colors.blue),
+                              ),
+                              Text(
+                                boarding[index]['description'],
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
-                                  Text(
-                                    boarding[index]['heading'],
-                                    style: const TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w800,
-                                        color: Colors.blue),
-                                  ),
-                                  Text(
-                                    boarding[index]['description'],
-                                    textAlign: TextAlign.center,
-                                    style: const TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      for (int i = 0; i < boarding.length; i++)
-                                        Container(
-                                          width: 5,
-                                          height: 5,
-                                          margin: const EdgeInsets.all(5),
-                                          decoration: BoxDecoration(
-                                              color: (i == index)
-                                                  ? Colors.black
-                                                  : Colors.black12,
-                                              shape: BoxShape.circle),
-                                        )
-                                    ],
-                                  ),
-                                  TextButton(
-                                      onPressed: () {
-                                        _pageController
-                                            .jumpToPage(boarding.length);
-                                      },
-                                      child: const Text(
-                                        "Skip",
-                                        style: TextStyle(
-                                            color: Colors.blue,
-                                            fontSize: 15,
-                                            fontWeight: FontWeight.w500),
-                                      )),
-                                  ButtonWidget(
-                                      buttonText: "Next",
-                                      buttonColor: Colors.blueAccent,
-                                      borderColor: Colors.blueAccent,
-                                      textColor: Colors.white,
-                                      onPressedFunction: () {
-                                        _pageController.nextPage(
-                                          duration:
-                                              const Duration(milliseconds: 500),
-                                          curve: Curves.ease,
-                                        );
-                                      }),
+                                  for (int i = 0; i < boarding.length; i++)
+                                    Container(
+                                      width: 5,
+                                      height: 5,
+                                      margin: const EdgeInsets.all(5),
+                                      decoration: BoxDecoration(
+                                          color: (i == index)
+                                              ? Colors.black
+                                              : Colors.black12,
+                                          shape: BoxShape.circle),
+                                    )
                                 ],
                               ),
-                            ),
+                              TextButton(
+                                  onPressed: () {
+                                    _pageController.jumpToPage(boarding.length);
+                                  },
+                                  child: const Text(
+                                    "Skip",
+                                    style: TextStyle(
+                                        color: Colors.blue,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.w500),
+                                  )),
+                              ButtonWidget(
+                                  buttonText: "Next",
+                                  buttonColor: Colors.blueAccent,
+                                  borderColor: Colors.blueAccent,
+                                  textColor: Colors.white,
+                                  onPressedFunction: () {
+                                    _pageController.nextPage(
+                                      duration:
+                                          const Duration(milliseconds: 500),
+                                      curve: Curves.ease,
+                                    );
+                                  }),
+                            ],
                           ),
-                        );
-                }));
+                        ),
+                      ),
+                    );
+            }));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:doctor_patient_management_system/Constants/AppConstants.dart';
 import 'package:doctor_patient_management_system/cubit/DoctorCubit/doctor_cubit.dart';
 import 'package:doctor_patient_management_system/cubit/UserCubit/user_cubit.dart';
@@ -9,6 +12,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import './Screens/SignUpFlow/BoardingScreen/BoardingScreen.dart';
 import './Screens/SignUpFlow/SignUpScreen/SignUpScreen.dart';
 import './Screens/SignUpFlow/WelcomeScreen/WelcomeScreen.dart';
+import 'Models/DoctorModel.dart';
+import 'Models/PatientModel.dart';
+import 'Screens/Doctor/LandingScreen/LandingScreen.dart';
+import 'Screens/Patient/LandingScreen/LandingScreen.dart';
+import 'Screens/SignUpFlow/RegisterUserRoleScreen/RegisterUserRolesScreen.dart';
 import 'cubit/LoadingCubit/loading_cubit.dart';
 import 'firebase_options.dart';
 
@@ -56,13 +64,137 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({
     super.key,
   });
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Map<String, dynamic> data = {};
+  bool gotCollectionData = false;
+  String role = "";
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchData(context: context);
+  }
+
+  Future fetchData({required BuildContext context}) async {
+    setState(() {
+      isLoading = true;
+    });
+    await FirebaseFirestore.instance
+        .collection('doctors')
+        .doc(FirebaseAuth.instance.currentUser?.uid)
+        .get()
+        .then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        log("${documentSnapshot.data()}", name: "doctor data");
+        if (documentSnapshot.exists) {
+          data = documentSnapshot.data() as Map<String, dynamic>;
+        }
+      }
+    });
+    if (data.isNotEmpty) {
+      if (context.mounted) {
+        setState(() {
+          gotCollectionData = true;
+          role = "doctor";
+          BlocProvider.of<DoctorCubit>(context)
+              .updateDoctorModel(singleDoctorModel: DoctorModel.fromMap(data));
+        });
+      }
+    }
+    if (gotCollectionData == false) {
+      await FirebaseFirestore.instance
+          .collection('patients')
+          .doc(FirebaseAuth.instance.currentUser?.uid)
+          .get()
+          .then((DocumentSnapshot documentSnapshot) {
+        if (documentSnapshot.exists) {
+          log("${documentSnapshot.data()}", name: "patient data");
+
+          data = documentSnapshot.data() as Map<String, dynamic>;
+        }
+      });
+      if (data.isNotEmpty) {
+        if (context.mounted) {
+          setState(() {
+            gotCollectionData = true;
+            role = "patient";
+          });
+          BlocProvider.of<PatientCubit>(context).updatePatientModel(
+              singlePatientModel: PatientModel.fromMap(data));
+        }
+      }
+    }
+    if (context.mounted) {
+      BlocProvider.of<UserCubit>(context).updateUserModel(
+        email: FirebaseAuth.instance.currentUser!.email.toString(),
+        uid: FirebaseAuth.instance.currentUser!.uid.toString(),
+        displayName: FirebaseAuth.instance.currentUser!.displayName.toString(),
+        photoUrl: FirebaseAuth.instance.currentUser!.photoURL.toString(),
+      );
+    }
+    setState(() {
+      isLoading = false;
+    });
+
+    if (gotCollectionData && role == "doctor" && isLoading == false) {
+      if (context.mounted) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => DoctorLandingScreen()),
+          (route) => false, // Close all existing routes
+        );
+      }
+    } else if (gotCollectionData && role == "patient" && isLoading == false) {
+      if (context.mounted) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => PatientLandingScreen()),
+          (route) => false, // Close all existing routes
+        );
+      }
+      return PatientLandingScreen();
+    } else if (role == "" && isLoading == false) {
+      if (context.mounted) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => RegisterUserRoleScreen()),
+          (route) => false, // Close all existing routes
+        );
+      }
+    } else {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => BoardingScreen()),
+        (route) => false, // Close all existing routes
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return BoardingScreen();
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      padding: const EdgeInsets.all(20),
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Center(
+            child: CircularProgressIndicator(),
+          )
+        ],
+      ),
+    );
   }
 }
